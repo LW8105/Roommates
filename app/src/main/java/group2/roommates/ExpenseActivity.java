010ebef05a97ceacef0bd9495f46031115f29968
diazbraulio <mr.brauliodiaz@gmail.com>
2015-05-03 20:29:38 -0700

    Expense Activity - Ability to Edit Expenses

--- app/src/main/java/group2/roommates/ExpenseActivity.java
+++ app/src/main/java/group2/roommates/ExpenseActivity.java
@@ -41,6 +41,7 @@ public class ExpenseActivity extends ActionBarActivity {
     Spinner categorySpinner;
     double roommateCount;
     DecimalFormat df = new DecimalFormat("#.##");
+    ExpenseObject object;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -192,7 +193,7 @@ public class ExpenseActivity extends ActionBarActivity {
         if (v.getId() == R.id.expenseListView) {
             ListView expenseListView = (ListView) v;
             AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-            ExpenseObject object = (ExpenseObject) expenseListView.getItemAtPosition(info.position);
+            object = (ExpenseObject) expenseListView.getItemAtPosition(info.position);
 
             menu.setHeaderTitle(object.getExpenseName());
 
@@ -210,10 +211,119 @@ public class ExpenseActivity extends ActionBarActivity {
         String menuItemName = menuItems[menuItemIndex];
         String listItemName = expenseArray.get(info.position).toString();
 
-        Log.v("Menu option selected", "edit");
 
 //        TextView text = (TextView) findViewById(R.id.footer);
 //        text.setText(String.format("Selected %s for item %s", menuItemName, listItemName));
+
+        switch (menuItemIndex) {
+            case 0:
+                Log.i("Menu option selected", "edit");
+                LayoutInflater inflater = LayoutInflater.from(this);
+                buildAlertDialog(inflater, object);
+                break;
+
+            case 1:
+                Log.i("Menu option selected", "delete");
+                break;
+
+            case 2:
+                ListAdapter adapter = new ExpenseAdapter(ExpenseActivity.this, R.layout.expense_row, expenseArray);
+                ListView expenseListView = (ListView) findViewById(R.id.expenseListView);
+
+                object.setPaid(!object.isPaid());
+                expenseListView.setAdapter(adapter);
+                break;
+        }
+
         return true;
+    }
+
+    public void buildAlertDialog(LayoutInflater inflater, final ExpenseObject object) {
+        final View ADD_EXPENSE_VIEW = inflater.inflate(R.layout.expense_dialog, null);
+
+        DATE_INPUT = (EditText) ADD_EXPENSE_VIEW.findViewById(R.id.editDueDate);
+        final EditText AMOUNT_INPUT = (EditText) ADD_EXPENSE_VIEW.findViewById(R.id.editAmount);
+        final EditText DESCRIPTION_INPUT = (EditText) ADD_EXPENSE_VIEW.findViewById(R.id.editNotes);
+
+
+        AMOUNT_INPUT.setText(object.getExpenseAmount(), TextView.BufferType.EDITABLE);
+        DESCRIPTION_INPUT.setText(object.getNotes(), TextView.BufferType.EDITABLE);
+        DATE_INPUT.setText(object.getDueDate(), TextView.BufferType.EDITABLE);
+
+        spinnerAdapter = ArrayAdapter.createFromResource(ExpenseActivity.this,
+                R.array.description_array, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Add New Expense");
+        dialogBuilder.setView(ADD_EXPENSE_VIEW);
+
+        categorySpinner = (Spinner) ADD_EXPENSE_VIEW.findViewById(R.id.categorySpinner);
+        categorySpinner.setAdapter(spinnerAdapter);
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 4) {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        dialogBuilder.setPositiveButton("EDIT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                double divide = Double.parseDouble(AMOUNT_INPUT.getText().toString()) / roommateCount;
+                DecimalFormat df = new DecimalFormat("#.00");
+
+//                ExpenseObject newExpense = new ExpenseObject(0, AMOUNT_INPUT.getText().toString(), divide, dueDate,
+//                        categorySpinner.getSelectedItem().toString(), LoginActivity.getUserName(), DESCRIPTION_INPUT.getText().toString(), false);
+//                expenseArray.add(newExpense);
+
+                object.setExpenseName(categorySpinner.getSelectedItem().toString());
+                object.setExpenseAmount(AMOUNT_INPUT.getText().toString());
+                object.setDividedAmount(Double.valueOf(df.format(divide)));
+                object.setDueDate(DATE_INPUT.getText().toString());
+                object.setNotes(DESCRIPTION_INPUT.getText().toString());
+
+                ListAdapter adapter = new ExpenseAdapter(ExpenseActivity.this, R.layout.expense_row, expenseArray);
+                ListView expenseListView = (ListView) findViewById(R.id.expenseListView);
+
+                expenseListView.setAdapter(adapter);
+                registerForContextMenu(expenseListView);
+            }
+        });
+
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialogBuilder.show();
+
+        final DatePickerDialog.OnDateSetListener dueDatePicker = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                MY_CALENDAR.set(Calendar.YEAR, year);
+                MY_CALENDAR.set(Calendar.MONTH, monthOfYear);
+                MY_CALENDAR.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        DATE_INPUT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ExpenseActivity.this, dueDatePicker, MY_CALENDAR.get(Calendar.YEAR),
+                        MY_CALENDAR.get(Calendar.MONTH), MY_CALENDAR.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/group2/roommates/ExpenseObject.java
+++ app/src/main/java/group2/roommates/ExpenseObject.java
@@ -27,31 +27,65 @@ public class ExpenseObject {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getExpenseAmount() {
         return expenseAmount;
+    }
+
+    public void setExpenseAmount(String expenseAmount) {
+        this.expenseAmount = expenseAmount;
     }
 
     public double getDividedAmount() {
         return dividedAmount;
     }
 
+    public void setDividedAmount(double dividedAmount) {
+        this.dividedAmount = dividedAmount;
+    }
+
     public String getDueDate() {
         return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
     }
 
     public String getAuthor() {
         return author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
     public String getExpenseName() {
         return expenseName;
+    }
+
+    public void setExpenseName(String expenseName) {
+        this.expenseName = expenseName;
     }
 
     public String getNotes() {
         return notes;
     }
 
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
     public boolean isPaid() {
         return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
     }
 }
